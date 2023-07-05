95eb99e4ebac0fb14172d9b1b397460a740152d7
AlfredHutomo <alfredhutomo@gmail.com>
2023-07-06 04:39:13 +0700

    added simple change date

--- src/components/ControlBar.tsx
+++ src/components/ControlBar.tsx
@@ -1,15 +1,38 @@
+import { usePathname } from "next/navigation";
 import ThemeSwitcher from "./ThemeSwitcher";
+import { useEffect, useState } from "react";
+
+const birthMonth = 7;
+const birthYear = 1999;
 
 const ControlBar = () => {
+  const pathname = usePathname();
+  const [month, setMonth] = useState(birthMonth);
+  const [year, setYear] = useState(birthYear);
+
+  useEffect(() => {
+    if (pathname !== "/") {
+      const date = new Date();
+      const currentMonth = date.getMonth() + 1;
+      const currentYear = date.getFullYear();
+      setMonth(currentMonth);
+      setYear(currentYear);
+    } else {
+      setMonth(birthMonth);
+      setYear(birthYear);
+    }
+  }, [pathname]);
+
   return (
     <nav className="flex p-7 justify-between sm:flex-col sm:w-[100px] sm:px-0 sm:py-[40px] sm:items-center col-span-1 text-jeans dark:text-water text-xl">
-      <p className="w-28 sm:w-fit">7</p>
+      <p className="w-28 sm:w-fit">{month}</p>
       <ThemeSwitcher />
-      <div className="flex flex-row sm:flex-col justify-end items-center gap-0 w-28 sm:w-fit">
-        <p>1</p>
-        <p>9</p>
-        <p>9</p>
-        <p>9</p>
+      <div className="flex flex-row sm:flex-col justify-between items-center w-28 sm:w-fit sm:h-[130px]">
+        {String(year)
+          .split("")
+          .map((digit, index) => (
+            <p key={index}>{digit}</p>
+          ))}
       </div>
     </nav>
   );
