6722d9533bef0b5a05b48485369253fa77bdfe42
AlfredHutomo <alfredhutomo@gmail.com>
2023-07-06 15:19:43 +0700

    make projects more maintainable

--- src/app/works/page.tsx
+++ src/app/works/page.tsx
@@ -3,6 +3,38 @@ export const metadata = {
   description: "Alfred Hutomo's works and experiences.",
 };
 
+const projects: { name: string; link?: string }[] = [
+  {
+    name: "Attendance Summary App for a small business",
+    link: "https://asa.alfredtomo.dev",
+  },
+  {
+    name: "Doctor Booking App",
+  },
+  {
+    name: "Giphy API Integration App",
+    link: "https://thriving-marshmallow-318cf2.netlify.app/",
+  },
+  {
+    name: "Portfolio Website V1",
+    link: "https://v1.alfredtomo.dev/",
+  },
+  {
+    name: "Real Futbol Institute Website with Relab Studios",
+    link: "https://www.realfutbolinstitute.com/",
+  },
+  {
+    name: "16th Indonesian Film Festival with Relab Studios",
+    link: "https://iffaustralia.com",
+  },
+  {
+    name: "15th 2.0 Indonesian Film Festival",
+  },
+  {
+    name: "Quiz plus RoR App",
+  },
+];
+
 export default function Works() {
   return (
     <main className="page">
@@ -27,25 +59,26 @@ export default function Works() {
           Australia - (2020 - 2021)
         </p>
         <h3>Projects</h3>
-        <a className="font-bold underline" href="https://asa.alfredtomo.dev">
-          Attendance Summary App
-        </a>
-        <p className="font-bold">Doctor Booking App</p>
-        <a className="font-bold underline" href="https://v1.alfredtomo.dev/">
-          Portfolio Website V1
-        </a>
-        <a
-          className="font-bold underline"
-          href="https://www.realfutbolinstitute.com/"
-        >
-          Real Futbol Institute Website
-        </a>
-        <a className="font-bold underline" href="https://iffaustralia.com/">
-          16th Indonesian Film Festival partnering with Relab Studios
-        </a>
-        <p className="font-bold">15th 2.0 Indonesian Film Festival</p>
-        <p className="font-bold">Quiz plus</p>
-        <h6>Exciting Projects Coming Soon</h6>
+        {projects.map((project) => {
+          if (project.link) {
+            return (
+              <a
+                key={project.name}
+                className="font-bold underline"
+                href={project.link}
+                target="_blank"
+              >
+                {project.name}
+              </a>
+            );
+          }
+          return (
+            <p key={project.name} className="font-bold">
+              {project.name}
+            </p>
+          );
+        })}
+        <h6>Exciting Projects Revealed Soon</h6>
       </section>
     </main>
   );
