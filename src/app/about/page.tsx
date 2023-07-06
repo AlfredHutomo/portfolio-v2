3b536c2188ae4cbfc72fbcf482ed6b6f0664ad32
AlfredHutomo <alfredhutomo@gmail.com>
2023-07-06 19:57:17 +0700

    update about headers

--- src/app/about/page.tsx
+++ src/app/about/page.tsx
@@ -14,7 +14,7 @@ export default function About() {
           in full stack development. My skill set encompasses both frontend and
           backend development, cloud computing, DevOps, and project management.
         </p>
-        <h3>Technical Expertise</h3>
+        <h3>Technicalities</h3>
         <ul>
           <li>Languages: JavaScript, TypeScript, Python, Java, and C#</li>
           <li>Frameworks & Libraries: Node, React, Next.</li>
@@ -23,14 +23,14 @@ export default function About() {
             Extension skills: functional programming and mobile app development.
           </li>
         </ul>
-        <h3>Passion & Drive</h3>
+        <h3>Passions & Drives</h3>
         <p>
           Beyond my technical abilities, I&apos;m driven by my hobbies for
           gaming, photography, videography, and snowboarding. These interests
           showcase my creativity and desire for excellence, which I bring to
           every project I work on.
         </p>
-        <h3>Education</h3>
+        <h3>Educations</h3>
         <p>
           I hold a Bachelor&apos;s degree in Information Technology with
           Distinction, specializing in Software System Development, from RMIT
